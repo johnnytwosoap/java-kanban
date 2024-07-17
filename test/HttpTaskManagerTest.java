33e9bc1ca0a4f03e7b8952d1a5b7407e7fa54cf4
johnnydvaroyala <evgenia_kretova@mail.ru>
2024-07-17 21:47:52 +0300

    first commit 9 sprint+

--- test/HttpTaskManagerTest.java
+++ test/HttpTaskManagerTest.java
@@ -36,7 +36,9 @@ public class HttpTaskManagerTest {
         assertTrue(defaultManager.getAllSubTasks().isEmpty(), "Задача найдена.");
 
         Task task = new Task("first task","firstTask description", TaskStatus.DONE, null, 0);
-        httpTaskClient.client("tasks", task);
+        String firstTask = httpTaskClient.client("tasks", task).replace("\\","");
+        JsonElement jsonElementTask = JsonParser.parseString(firstTask.substring(1, firstTask.length() - 1));
+        Integer firstTaskId = jsonElementTask.getAsJsonObject().get("id").getAsInt();
         assertEquals(1, defaultManager.getAllTasks().size(), "Задача не найдена.");
 
 
@@ -46,7 +48,7 @@ public class HttpTaskManagerTest {
         assertEquals(1, defaultManager.getAllEpic().size(), "Задача не найдена.");
 
 
-        JsonElement jsonElement = JsonParser.parseString(firstEpic.substring(1,firstEpic.length()-1));
+        JsonElement jsonElement = JsonParser.parseString(firstEpic.substring(1, firstEpic.length() - 1));
         JsonObject jsonObject = jsonElement.getAsJsonObject();
         Integer firstEpicId = jsonObject.get("id").getAsInt();
         SubTask subTask = new SubTask(
@@ -55,17 +57,20 @@ public class HttpTaskManagerTest {
                 TaskStatus.DONE, firstEpicId,
                 LocalDateTime.parse("28-06-2024 03:04:05", formatter),
                 10);
-        httpTaskClient.client("subtasks", subTask);
+        String firstSubTask = httpTaskClient.client("subtasks", subTask).replace("\\","");
+
+        JsonElement jsonElementSubTask = JsonParser.parseString(firstSubTask.substring(1, firstSubTask.length() - 1));
+        Integer firstSubTaskId = jsonElementSubTask.getAsJsonObject().get("id").getAsInt();
 
         assertEquals(1, defaultManager.getAllSubTasks().size(), "Задача не найдена.");
 
 
 
-        httpTaskClient.clientGetDelete("tasks/1","DELETE");
+        httpTaskClient.clientGetDelete("tasks/" + firstTaskId,"DELETE");
 
-        httpTaskClient.clientGetDelete("epics/2","DELETE");
+        httpTaskClient.clientGetDelete("epics/" + firstEpicId,"DELETE");
 
-        httpTaskClient.clientGetDelete("subtasks/3","DELETE");
+        httpTaskClient.clientGetDelete("subtasks/" + firstSubTaskId,"DELETE");
 
 
         assertTrue(defaultManager.getAllTasks().isEmpty(), "Задача найдена.");
